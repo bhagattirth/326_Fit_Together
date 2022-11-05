aecbe24e4f8721936d347fca7e4971f599eed544
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-11-05 18:16:08 -0400

    implemented basic functionality for homepage and login page scripts

---
+++ client/src/login.js
@@ -0,0 +1,59 @@
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+const loginBtn = document.getElementById("login");
+const signupBtn = document.getElementById("signup");
+
+// email validation
+const validateEmail = (email) => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+};
+
+// password validation
+const validatePassword = (password) => {
+	const re = /^(?=.*[0-9_\W]).+$/;
+	return re.test(password);
+	// 8 char long, lowercase, uppercase, special char or number
+};
+
+// login user
+const loginUser = (e) => {
+	if (e.target.tagName !== "BUTTON") return;
+	e.stopPropagation();
+
+	if (!validateEmail(email.value)) {
+		alert("invalid email");
+		return;
+	}
+
+	if (!validatePassword(pass.value)) {
+		alert("invalid password");
+		return;
+	}
+
+	// make it here, post req to backend
+	// then redirect upon success
+};
+
+// sign up user
+const signupUser = (e) => {
+	if (e.target.tagName !== "BUTTON") return;
+	e.stopPropagation();
+
+	if (!validateEmail(email.value)) {
+		alert("invalid email");
+		return;
+	}
+
+	if (!validatePassword(pass.value)) {
+		alert("invalid password");
+		return;
+	}
+
+	// make it here, create post request to backend
+	// then redirect upon success`
+};
+
+signupBtn.addEventListener("click", signupUser);
+loginBtn.addEventListener("click", loginUser);
