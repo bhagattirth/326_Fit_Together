01143336ad4d32a0ee0342569ed70d9caa37aea7
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-11-06 16:50:23 -0500

    added basic auth flow

--- client/src/login.js
+++ client/src/login.js
@@ -18,51 +18,84 @@ const validatePassword = (password) => {
 };
 
 // login user
-const loginUser = (e) => {
-	if (e.target.tagName !== "BUTTON") return;
+const loginUser = async (e) => {
+	if (e.target.id !== "login") return;
 	e.stopPropagation();
 
+	// email validation
 	if (!validateEmail(email.value)) {
 		alert("invalid email");
 		return;
 	}
 
+	// password validation
 	if (!validatePassword(pass.value)) {
 		alert("invalid password");
 		return;
 	}
 
-	// make it here, post req to backend
-	// then redirect upon success
+	// attempt to log user in
+	try {
+		const res = await fetch("http://localhost:5000/auth/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ email: email.value, password: pass.value }),
+		});
+		const msg = await res.json();
+
+		if (!res.ok) {
+			throw new Error("Invalid email/password combination");
+		}
+	} catch (err) {
+		const text = document.getElementById("error-text");
+		text.hidden = false;
+		return;
+	}
+
+	// redirect back to homepage here
+	location.href = "index.html";
 };
 
 // sign up user
 const signupUser = async (e) => {
-	if (e.target.tagName !== "BUTTON") return;
+	if (e.target.id !== "signup") return;
 	e.stopPropagation();
 
+	// email validation
 	if (!validateEmail(email.value)) {
 		alert("invalid email");
 		return;
 	}
 
+	// password validation
 	if (!validatePassword(pass.value)) {
 		alert("invalid password");
 		return;
 	}
 
-	const res = await fetch("http://localhost:5000/auth/signup", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ email: email.value, password: pass.value }),
-	});
-	const msg = await res.json();
-	console.log(msg);
+	// attempt to sign user up
+	try {
+		const res = await fetch("http://localhost:5000/auth/signup", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ email: email.value, password: pass.value }),
+		});
+		const msg = await res.json();
 
-	// make it here, create post request to backend
-	// then redirect upon success`
+		if (!res.ok) {
+			throw new Error("Something went wrong please try again later");
+		}
+	} catch (err) {
+		//display dropdown
+		return;
+	}
+
+	//redirect user back to homepage here
+	location.href = "index.html";
 };
 
 signupBtn.addEventListener("click", signupUser);
--- server/controllers/login-controller.js
+++ server/controllers/login-controller.js
@@ -1,7 +1,21 @@
 import bcrypt from "bcrypt";
 
-export const loginUser = (req, res, next) => {
+export const loginUser = async (req, res, next) => {
 	// do something to log user in
+	const { email, password } = req.body;
+
+	// look for user with matching email, if not found return error
+
+	if (false) {
+		res.status(400).send({ message: "User doesn't exist" });
+	}
+
+	// with found user, check that password matches, if not return error
+	const valid = await bcrypt.compare(password, "DATABASE PASSWORD");
+
+	if (!valid) {
+		res.status(400).send({ message: "Invalid password" });
+	}
 };
 
 export const signupUser = async (req, res, next) => {
