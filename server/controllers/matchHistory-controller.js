4e658341556e4ba3d4d8a5c2787f33543e643447
Tirth Bhagat <76080774+bhagattirth@users.noreply.github.com>
2022-11-20 14:12:53 -0500

    Finished redoing the front end connection - still need to test

--- server/controllers/matchHistory-controller.js
+++ server/controllers/matchHistory-controller.js
@@ -3,7 +3,7 @@ import {User} from "../app.js";
 
 
 export const getPastUser = (req, res, next)=>{
-    const {id} = req.params;
+    const userId = req.params.id;
     
     User.findOne({id:"wqrlajf"}, (err, foundItem) =>{
 		if(err){
@@ -16,11 +16,33 @@ export const getPastUser = (req, res, next)=>{
 	});
 };
 
+export const getProfileInfo = (req, res, next)=>{
+    const userId = req.params.id;
+    
+    User.findOne({id:"wqrlajf"}, (err, foundItem) =>{
+		if(err){
+            res.status(404).send({ message: err });
+        }else if(!foundItem){
+            res.status(400).send({ message: "User doesn't exist" })
+        }else{
+            const info = {
+                imgURL:foundItem.profilePic, name:foundItem.fName + " " + foundItem.lName,
+                preference:foundItem.prefDays, email:foundItem.email
+            };
+            res.status(200).send(JSON.stringify(info))
+        }
+	});
+};
+
+
 export const addWorkoutToUser = (req, res, next) =>{
 
+    const {user, member, dates, type} = req.body;
+    let workout = req.body.workout;
+    const list = workout.replace(/ /g, " ").split(",");
     const filter = {"$and": [{"id": "wqrlajf"}, {'pastWorkouts.id': "dsafag"}]};
-    const update = {"$push": {"pastWorkouts.$.workout":["test"]}};
-
+    const update = {"$push": {"pastWorkouts.$.workout":list, "pastWorkouts.$.workoutTitle":type, "pastWorkouts.$.date":dates}};
+    
     User.findOneAndUpdate(filter, update, (err, foundItem) => { 
         if(err){
             res.status(404).send(err);
@@ -34,8 +56,9 @@ export const addWorkoutToUser = (req, res, next) =>{
 
 export const updateRating = (req, res, next) =>{
 
+    const {user, member, rating} = req.body;
     const filter = {"$and": [{"id": "wqrlajf"}, {'pastWorkouts.id': "dsafag"}]};
-    const update = {"$set": {"pastWorkouts.$.rating":"5"}};
+    const update = {"$set": {"pastWorkouts.$.rating": rating}};
 
     User.findOneAndUpdate(filter, update, (err, foundItem) => { 
         if(err){
@@ -51,6 +74,7 @@ export const updateRating = (req, res, next) =>{
 
 export const deleteEntry = (req, res, next)=>{
     
+    const {user, member} = req.body;
     const filter = {"$and": [{"id": "wqjf"}, {'pastWorkouts.id': "dsafag"}]};
     const update = {"$pull": {"pastWorkouts":{id:"dsafag"}}};
 
--- server/routes/matchHistory-routes.js
+++ server/routes/matchHistory-routes.js
@@ -5,11 +5,14 @@ import {
 	updateRating,
 	deleteEntry,
 	getPastUser,
+	getProfileInfo
 } from "../controllers/matchHistory-controller.js";
 
 let router = express.Router();
 
 router.get("/:id/getPast", getPastUser);
+
+router.get("/:id/getProfileInfo", getProfileInfo);
 
 router.post("/addWorkout", addWorkoutToUser);
 
