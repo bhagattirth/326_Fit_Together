2fd2354591692b6799c62e240d6f0c80578d8283
Tirth Bhagat <76080774+bhagattirth@users.noreply.github.com>
2022-12-02 00:35:57 -0500

    Added Some Comments

--- server/public/src/matchHistory.js
+++ server/public/src/matchHistory.js
@@ -1,12 +1,15 @@
 import user from "./user.js";
 const urlBase = "http://localhost:5000";
+
+//Keeps track of the members ids that are being displayed for the user
 let userMap = { user1: null, user2: null, user3: null };
+
+//Indexes to keep track of prev and next page
 let firstEntryOnPage = 0;
 let lastEntryOnPage = 2;
-let jsonSize = 0;
-const profilePictureImage = document.getElementById("profilePicture");
 
-await validateUser();
+//Used for prev and next page to determine if there is a next page
+let jsonSize = 0;
 
 async function validateUser() {
 	// validate accessToken
@@ -67,25 +70,13 @@ async function validateUser() {
 	logoutBtn.addEventListener("click", user.logout);
 }
 
-async function logout() {
-	const res = await fetch(`${urlBase}/auth/logout`, {
-		method: "POST",
-		credentials: "include",
-		headers: { "Content-type": "application/json" },
-		body: null,
-	});
-
-	const msg = await res.json();
-
-	if (res.ok) {
-		location.href = "index.html";
-	} else {
-		alert("failed to logout");
-	}
-}
-
+//The main function - Loads Past Member Entries into the page
 const loadUsers = async (id) => {
+
+	//The array of past workouts of the user that is fetch from the database
 	let json = null;
+
+	//Attempts to fetch the pastworkout array
 	try {
 		const res = await fetch(`${urlBase}/matchHistory/${id}/getPast`, {
 			method: "GET",
@@ -101,26 +92,44 @@ const loadUsers = async (id) => {
 		return;
 	}
 
+	//resets the map
 	userMap = { user1: null, user2: null, user3: null };
-
 	jsonSize = json.length;
 
+	//Will go from 1 upto 3 - used to add things into usermap
 	let count = 1;
+
+	//Grabs firstEntryOnPageth member in the array upto lastEntryOnPageth member (at most 3)
 	let i = firstEntryOnPage;
 	while (i <= lastEntryOnPage && i < json.length) {
-		const userInfo = await getUserInfo(json[i].id);
-
+		//Grabs essential member data used for displaying
+		const userInfo = await getMembersInfo(json[i].id);
+		//adds member id to the userMap to make it easier to do api calls
 		userMap["user" + count] = json[i].id;
+		//Uses Essential member data to display it on one of the entries
 		addProfileInfo(userInfo, count);
+		//Creates carousel for a spefic entry
 		createCarousel(json[i], count);
+		//Displays current user's rating for the member
 		showRating(json[i], count);
 		i++;
 		count++;
 	}
+	//Hide or reveal entry slots depending on how many users are going to be displayed
 	displayUser();
 };
 
-async function getUserInfo(id) {
+
+/*
+	Takes member's id and returns a dictionary of member info needed for displaying
+	member info:{
+		imgURL: Image URL
+		name: Member's first and last name,
+		preference: Member's prefered workout days,
+		email: member's email,
+	}
+*/
+async function getMembersInfo(id) {
 	try {
 		const res = await fetch(
 			`${urlBase}/matchHistory/${id}/getProfileInfo`,
@@ -141,6 +150,7 @@ async function getUserInfo(id) {
 	}
 }
 
+//Uses member info to display it on the ith entry
 function addProfileInfo(userData, i) {
 	let profileInfo = document.getElementById("profile" + i);
 	let profilePic = document.getElementById("profilePic" + i);
@@ -153,13 +163,14 @@ function addProfileInfo(userData, i) {
 	profileInfo.innerHTML = "";
 	profileInfo.appendChild(name);
 	profileInfo.innerHTML += "</br>";
-	// profileInfo.innerHTML+= "Last Workout On: " + userData.lastWorkout;
 	profileInfo.innerHTML += "</br>";
 	profileInfo.innerHTML += "Contact: " + userData.email;
 	profilePic.src = userData.imgURL;
 
+	//Resets all Date Text if it exist
 	prefDate.innerText = "";
 
+	//If Member has prefered day set
 	if (userData.preference.length > 0) {
 		for (let i = 0; i < userData.preference.length; i++) {
 			if (userData.preference.length - 1 === i) {
@@ -302,6 +313,7 @@ function createWorkoutCard(exercises, title, date, cBody, isFirst) {
 	cBody.appendChild(carouselItem);
 }
 
+//D
 function showRating(userData, i) {
 	let dropDown = document.getElementById("selectRating" + i);
 	dropDown.innerHTML = "";
@@ -319,12 +331,14 @@ function showRating(userData, i) {
 	});
 }
 
+//Helper Function Used to add multiple attributes at once
 function setAttributes(el, attrs) {
 	for (var key in attrs) {
 		el.setAttribute(key, attrs[key]);
 	}
 }
 
+//Button Event To add a valid workout to a entry
 const addWorkoutListner = (i) => async () => {
 	let workouts = document.getElementById("workout" + i).value;
 	let date = document.getElementById("date" + i).value;
@@ -363,6 +377,7 @@ const addWorkoutListner = (i) => async () => {
 	}
 };
 
+//Button Event To Update a members rating
 const updateRatingListiner = (i) => async () => {
 	let newRating = document.getElementById("selectRating" + i).value;
 	const res = await fetch(`${urlBase}/matchHistory/ratingUpdate`, {
@@ -384,6 +399,7 @@ const updateRatingListiner = (i) => async () => {
 	}
 };
 
+//Button Event Used to Delete User From User workout record
 const deleteUserListiner = (i) => async () => {
 	let userI = "user" + i;
 
@@ -407,6 +423,7 @@ const deleteUserListiner = (i) => async () => {
 	}
 };
 
+//Displays entry slots if there is member data on that slot
 function displayUser() {
 	if (userMap["user1"] !== null) {
 		document.getElementById("previousMatchesOne").style.display = "";
@@ -427,31 +444,53 @@ function displayUser() {
 	}
 }
 
+
+await validateUser();
+//Loads users past workout entries
 loadUsers(user.getUserId());
 
+//Allows user to update rating when pressing the update button for the first entry
 document
 	.getElementById("updateRating1")
 	.addEventListener("click", updateRatingListiner(1));
+
+//Allows user to add workout when adding a valid workout for entry 1 when clicking the button
 document.getElementById("add1").addEventListener("click", addWorkoutListner(1));
+
+//Removes entry 1 when clicking this button
 document
 	.getElementById("removeMatch1")
 	.addEventListener("click", deleteUserListiner(1));
 
+
+//Allows user to update rating when pressing the update button for the second entry
 document
 	.getElementById("updateRating2")
 	.addEventListener("click", updateRatingListiner(2));
+
+//Allows user to add workout when adding a valid workout for entry 2 when clicking the button
 document.getElementById("add2").addEventListener("click", addWorkoutListner(2));
+
+//Removes entry 2 when clicking the remove button
 document
 	.getElementById("removeMatch2")
 	.addEventListener("click", deleteUserListiner(2));
 
+
+//Allows user to update rating when pressing the update button for the third entry
 document
 	.getElementById("updateRating3")
 	.addEventListener("click", updateRatingListiner(3));
+
+//Allows user to add workout when adding a valid workout for entry 2 when clicking the button
 document.getElementById("add3").addEventListener("click", addWorkoutListner(3));
+
+//Removes entry 3 when clicking the remove button
 document
 	.getElementById("removeMatch3")
 	.addEventListener("click", deleteUserListiner(3));
 
+
+//Enables buttons the prev and next buttons to go back and forth on pages
 document.getElementById("next").addEventListener("click", getNextPage);
 document.getElementById("back").addEventListener("click", prevPage);
