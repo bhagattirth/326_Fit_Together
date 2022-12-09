6cd1c69def5eadf75727848f93d9793fb5523ec6
Tirth Bhagat <76080774+bhagattirth@users.noreply.github.com>
2022-12-08 22:32:24 -0500

    Backend is 95% done

--- server/public/src/matchHistory.js
+++ server/public/src/matchHistory.js
@@ -403,7 +403,7 @@ const addWorkoutListner = (i) => async () => {
 		workouts === "" ||
 		date === "" ||
 		wType === "" ||
-		!date.match(/^\d{2}[./-]\d{2}[./-]\d{4}$/)
+		!date.match(/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/]\d{4}$/)
 	) {
 		alert("Please Fill in the Fields Correctly");
 	} else {
